90328ed0a8035a4126ebb8dee0ae5592861c4a13
T.C <travis18@gmail.com>
2018-09-03 04:26:00 +0800

    Setup project routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import { routes } from "./routes";
+
+//install VueRouter to Vue
+Vue.use(VueRouter)
+
+//setup routes to VueRouter
+var router = new VueRouter({
+  mode: "history",
+  routes
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
